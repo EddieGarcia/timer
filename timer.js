8c53b57544f69a346b8b56805f9f5c646a0cc083
Eduard Kubinec <eduard.kubinec@sap.com>
2023-08-20 19:46:53 +0200

    adjust script

--- timer.js
+++ timer.js
@@ -1,7 +1,14 @@
 
 let targetTimeM = 20;
-let serviceInH = 23;
-let serviceInM = 7;
+const currentTime = new Date();
+let serviceInH = currentTime.getHours();
+let serviceInM = currentTime.getMinutes();
+
+const currentTimeDiv = document.getElementById("current-time");
+const serviceInDiv = document.getElementById("service-in");
+const targetTimeDiv = document.getElementById("target-time");
+const serviceOutDiv = document.getElementById("service-out");
+const countdownDiv = document.getElementById("countdown");
 
 const toPaddedStr = (num) => num.toString().padStart(2, '0');
 const trimToRangeCyclic = (value, upperBound) => (value + upperBound) % upperBound;
@@ -12,18 +19,18 @@ const diffTime = (dateFuture, date) => {
         hours: Math.floor((diffSeconds / 3600) % 24),
         minutes: Math.floor((diffSeconds / 60) % 60),
         seconds: Math.floor(diffSeconds % 60)
-    }
+    };
 };
 
 const adjustServiceInH = (step) => {
     serviceInH = trimToRangeCyclic(serviceInH + step, 24);
     render();
-}
+};
 
 const adjustServiceInM = (step) => {
     serviceInM = trimToRangeCyclic(serviceInM + step, 60);
     render();
-}
+};
 
 const adjustTargetTime = (step) => {
     targetTimeM = trimToRangeCyclic(targetTimeM + step, 100);
@@ -32,44 +39,37 @@ const adjustTargetTime = (step) => {
 
 const render = () => {
     // current time
-    const ct = new Date();
-    const cthours = toPaddedStr(ct.getHours());
-    const ctminutes = toPaddedStr(ct.getMinutes());
-    const ctseconds = toPaddedStr(ct.getSeconds());
-    document.getElementById("current-time").innerHTML = `${cthours}:${ctminutes}:${ctseconds}`;
+    const currentTime = new Date();
+    currentTimeDiv.innerHTML = `${toPaddedStr(currentTime.getHours())}:${toPaddedStr(currentTime.getMinutes())}:${toPaddedStr(currentTime.getSeconds())}`;
 
     //service in
-    const serviceIn = new Date(ct.getTime());
+    const serviceIn = new Date(currentTime.getTime());
     serviceIn.setHours(serviceInH, serviceInM, 0);
-    document.getElementById("service-in").innerHTML = `${toPaddedStr(serviceInH)}:${toPaddedStr(serviceInM)}`;
+    serviceInDiv.innerHTML = `${toPaddedStr(serviceInH)}:${toPaddedStr(serviceInM)}`;
 
     // target time
-    document.getElementById("target-time").innerHTML = `${toPaddedStr(targetTimeM)}`;
+    targetTimeDiv.innerHTML = `${toPaddedStr(targetTimeM)}`;
 
     //servcie out
-    const serviceOut = new Date(ct.getTime());
+    const serviceOut = new Date(currentTime.getTime());
     serviceOut.setHours(serviceInH, serviceInM + targetTimeM, 0);
-    document.getElementById("service-out").innerHTML = `${toPaddedStr(serviceOut.getHours())}:${toPaddedStr(serviceOut.getMinutes())}`;
+    serviceOutDiv.innerHTML = `${toPaddedStr(serviceOut.getHours())}:${toPaddedStr(serviceOut.getMinutes())}`;
 
     //countdown
     let delta;
-    let color = "green";
-    if (ct < serviceIn) {
+    let color = "black";
+    if (currentTime < serviceIn) {
         delta = diffTime(serviceOut, serviceIn);
     } else {
-        if (serviceOut <= ct) {
-            delta = diffTime(ct, serviceOut);
+        if (serviceOut <= currentTime) {
+            delta = diffTime(currentTime, serviceOut);
             color = "red";
         } else {
-            delta = diffTime(serviceOut, ct);
+            delta = diffTime(serviceOut, currentTime);
         }
     }
-
-    const dhours = toPaddedStr(delta.hours);
-    const dminutes = toPaddedStr(delta.minutes);
-    const dseconds = toPaddedStr(delta.seconds);
-    document.getElementById("countdown").innerHTML = `${dhours}:${dminutes}:${dseconds}`;
-    document.getElementById("countdown").style.color = color;
+    countdownDiv.innerHTML = `${toPaddedStr(delta.hours)}:${toPaddedStr(delta.minutes)}:${toPaddedStr(delta.seconds)}`;
+    countdownDiv.style.color = color;
 };
 
 render();
